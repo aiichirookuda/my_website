e1f62139997b15890ec3bafc5961126d4be8925f
aiichiro <aiichirookuda@gmail.com>
2021-10-26 01:48:28 +0900

    basename追加

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { banner } from './data/banner';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/">
           <About />
